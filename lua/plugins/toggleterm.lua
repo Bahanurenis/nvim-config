027bb7a46a38f2210bddafadb6f211534ea77885
Bahanurenis <bahanurenis@gmail.com>
2024-06-19 09:30:42 +0200

    feat: init nvim config
    Signed-off-by: Bahanurenis <bahanurenis@gmail.com>

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,16 @@
+return {
+  {
+
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup(
+        {
+          open_mapping = [[<C-t>]],
+          terminal_mappings = false,
+        }
+      )
+    end,
+  },
+
+}
